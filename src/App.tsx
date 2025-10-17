440109b6435ffafe428dc5461b54146f95b28b88
Fernando Trejos <ferxo@MacBook-Pro-de-Fernando.local>
2025-10-17 17:16:30 -0500

    [init] button component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const countMore = () => {
+    setCount((count) => count + 1)
+  }
 
   return (
     <>
@@ -18,9 +23,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button label="count" parentMethod={countMore} />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
